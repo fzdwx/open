e772ad2542dd8de53a860a0f60c7ea0275eb0596
fzdwx <likelovec@gmail.com>
2022-09-15 16:10:26 +0800

    Update browser: add print url

--- pkg/browser/browser.go
+++ pkg/browser/browser.go
@@ -1,12 +1,17 @@
 package browser
 
 import (
+	"fmt"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/cli/go-gh/pkg/browser"
 	"os"
 )
 
 var (
-	b browser.Browser
+	b         browser.Browser
+	subtle    = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
+	highlight = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
+	special   = lipgloss.AdaptiveColor{Light: "#43BF6D", Dark: "#73F59F"}
 )
 
 func init() {
@@ -15,5 +20,15 @@ func init() {
 
 // Open url in browser
 func Open(url string) error {
-	return b.Browse(url)
+	err := b.Browse(url)
+
+	if err == nil {
+		fmt.Printf("%s %s %s",
+			lipgloss.NewStyle().Bold(true).Foreground(highlight).Render("√"),
+			"open",
+			lipgloss.NewStyle().Bold(true).Foreground(special).Render(url),
+		)
+	}
+
+	return err
 }
