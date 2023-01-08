ecf4abe88b00d856033bb4ccc73810503c996606
fzdwx <likelovec@gmail.com>
2023-01-08 16:07:36 +0800

    Adapt to the latest search flag of github

--- cmd/gh/gh.go
+++ cmd/gh/gh.go
@@ -28,12 +28,6 @@ $ open gh fzdwx -u -> https://github.com/search?q=fzdwx&type=users`,
 
 			searchType := getType()
 			if strx.IsNotBlank(searchType) {
-				if typeIssuesFlag {
-					url += " is:issue"
-				}
-				if typePrFlag {
-					url += " is:pr"
-				}
 				url += "&type=" + searchType
 			}
 
@@ -52,11 +46,10 @@ $ open gh fzdwx -u -> https://github.com/search?q=fzdwx&type=users`,
 		},
 	}
 
-	typeCodeFlag        bool
+	typeRepoFlag        bool
 	typeCommitsFlag     bool
 	typeIssuesFlag      bool
 	typePrFlag          bool
-	typeIssuesAndPrFlag bool
 	typeDiscussionsFlag bool
 	typePackagesFlag    bool
 	typeMarketplaceFlag bool
@@ -74,19 +67,18 @@ func Command() *cobra.Command {
 	gh.AddCommand(profile)
 	gh.AddCommand(repo)
 
-	gh.PersistentFlags().BoolVarP(&typeCodeFlag, "code", "c", typeCodeFlag, "set search type is code")
-	gh.PersistentFlags().BoolVarP(&typeCommitsFlag, "commits", "m", typeCommitsFlag, "set search type is commits")
+	gh.PersistentFlags().BoolVarP(&typeRepoFlag, "repo", "r", typeRepoFlag, "set search type is repositories")
 	gh.PersistentFlags().BoolVarP(&typeIssuesFlag, "issues", "i", typeIssuesFlag, "set search type is issues")
-	gh.PersistentFlags().BoolVarP(&typePrFlag, "pr", "p", typePrFlag, "set search type is pr")
-	gh.PersistentFlags().BoolVarP(&typeIssuesAndPrFlag, "issuesAndPr", "z", typeIssuesAndPrFlag, "set search type is issues and pull requests")
+	gh.PersistentFlags().BoolVarP(&typePrFlag, "pr", "p", typePrFlag, "set search type is pull requests")
 	gh.PersistentFlags().BoolVarP(&typeDiscussionsFlag, "discussions", "s", typeDiscussionsFlag, "set search type is discussions")
-	gh.PersistentFlags().BoolVarP(&typePackagesFlag, "packages", "g", typePackagesFlag, "set search type is packages")
-	gh.PersistentFlags().BoolVarP(&typeMarketplaceFlag, "marketplace", "r", typeMarketplaceFlag, "set search type is marketplace")
-	gh.PersistentFlags().BoolVarP(&typeTopicsFlag, "topics", "t", typeTopicsFlag, "set search type is topics")
-	gh.PersistentFlags().BoolVarP(&typeWikisFlag, "wikis", "w", typeWikisFlag, "set search type is wikis")
 	gh.PersistentFlags().BoolVarP(&typeUsersFlag, "users", "u", typeUsersFlag, "set search type is users")
+	gh.PersistentFlags().BoolVarP(&typeCommitsFlag, "commits", "c", typeCommitsFlag, "set search type is commits")
+	gh.PersistentFlags().BoolVarP(&typePackagesFlag, "packages", "g", typePackagesFlag, "set search type is packages")
+	gh.PersistentFlags().BoolVarP(&typeWikisFlag, "wikis", "w", typeWikisFlag, "set search type is wikis")
+	gh.PersistentFlags().BoolVarP(&typeTopicsFlag, "topics", "t", typeTopicsFlag, "set search type is topics")
+	gh.PersistentFlags().BoolVarP(&typeMarketplaceFlag, "marketplace", "m", typeMarketplaceFlag, "set search type is marketplace")
 
-	gh.MarkFlagsMutuallyExclusive("code", "commits", "issues", "pr", "issuesAndPr", "discussions", "packages", "marketplace", "topics", "wikis", "users")
+	gh.MarkFlagsMutuallyExclusive("repo", "commits", "issues", "pr", "discussions", "packages", "marketplace", "topics", "wikis", "users")
 
 	gh.PersistentFlags().BoolVarP(&statusOpen, "open", "o", typeWikisFlag, "search issues,pr status is open")
 	gh.PersistentFlags().BoolVarP(&statusClosed, "closed", "f", typeUsersFlag, "search issues,pr status is closed")
@@ -99,12 +91,14 @@ func Command() *cobra.Command {
 func getType() string {
 	var searchType string
 	switch {
-	case typeCodeFlag:
-		searchType = "code"
+	case typeRepoFlag:
+		searchType = "repositories"
 	case typeCommitsFlag:
 		searchType = "commits"
-	case typeIssuesFlag, typePrFlag, typeIssuesAndPrFlag:
+	case typeIssuesFlag:
 		searchType = "issues"
+	case typePrFlag:
+		searchType = "pullrequests"
 	case typeDiscussionsFlag:
 		searchType = "discussions"
 	case typePackagesFlag:
